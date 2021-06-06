801fde3b17821d591589b5ff21328afbd4356516
Arthur J Simas <arthursimas1@gmail.com>
2021-06-05 23:55:17 -0300

    skeletons

---
+++ src/Transforms.py
@@ -0,0 +1,18 @@
+class Transformation:
+    pass
+
+
+class Rotate(Transformation):
+    pass
+
+
+class Scale(Transformation):
+    pass
+
+
+class Shear(Transformation):
+    pass
+
+
+class Translate(Transformation):
+    pass
---
+++ src/main.py
@@ -0,0 +1,7 @@
+from Camera import Camera
+from Object import Object
+from Scene import Scene
+import Transforms
+
+if __name__ == '__main__':
+    pass
