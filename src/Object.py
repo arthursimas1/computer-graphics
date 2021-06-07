5eb98fb98f3edf9b9f306b4e818e747128c92630
caio-yabiku <kaiohys@hotmail.com>
2021-06-07 14:47:30 -0300

    Implementação inicial do Object

--- src/Object.py
+++ src/Object.py
@@ -1,2 +1,12 @@
 class Object:
-    pass
+    def __init__(self, path: str):
+        self.vertexes = []
+        self.faces = []
+        with open(path, 'r') as fp:
+            for line in fp.readlines():
+                line_type, *elements = line.split()
+
+                if line_type == 'v':
+                    self.vertexes.append(list(map(float, elements)))
+                elif line_type == 'f':
+                    self.faces.append(list(map(lambda x: int(x) - 1, elements)))
