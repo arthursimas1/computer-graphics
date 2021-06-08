cf1e919441b766f4adbf62d754585960a16c3176
Luiz Henrique Teixeira Mendes <luizhteixeiram@gmail.com>
2021-06-08 18:44:45 -0300

    Alterações na classe Transforms

--- src/Transforms.py
+++ src/Transforms.py
@@ -17,17 +17,12 @@ def rotate_x(vertex, rad: float):
     :return: Vertex after rotation.
     """
 
-    np_vertex = np.array([[vertex[0]],
-                          [vertex[1]],
-                          [vertex[2]],
-                          [1]])
-
     trans_matrix = np.array([[1,        0,         0, 0],
                              [0, cos(rad), -sin(rad), 0],
                              [0, sin(rad),  cos(rad), 0],
                              [0,        0,         0, 1]])
 
-    return np.matmul(trans_matrix, np_vertex)
+    return np.matmul(trans_matrix, vertex)
 
 
 def rotate_y(vertex, rad: float):
@@ -45,17 +40,12 @@ def rotate_y(vertex, rad: float):
     :return: Vertex after rotation.
     """
 
-    np_vertex = np.array([[vertex[0]],
-                          [vertex[1]],
-                          [vertex[2]],
-                          [1]])
-
     trans_matrix = np.array([[ cos(rad), 0, sin(rad), 0],
                              [        0, 1,        0, 0],
                              [-sin(rad), 0, cos(rad), 0],
                              [        0, 0,        0, 1]])
 
-    return np.matmul(trans_matrix, np_vertex)
+    return np.matmul(trans_matrix, vertex)
 
 
 def rotate_z(vertex, rad: float):
@@ -73,17 +63,12 @@ def rotate_z(vertex, rad: float):
     :return: Vertex after rotation.
     """
 
-    np_vertex = np.array([[vertex[0]],
-                          [vertex[1]],
-                          [vertex[2]],
-                          [1]])
-
     trans_matrix = np.array([[cos(rad), -sin(rad), 0, 0],
                              [sin(rad),  cos(rad), 0, 0],
                              [       0,         0, 1, 0],
                              [       0,         0, 0, 1]])
 
-    return np.matmul(trans_matrix, np_vertex)
+    return np.matmul(trans_matrix, vertex)
 
 
 def scale(vertex, factor: float):
@@ -103,17 +88,12 @@ def scale(vertex, factor: float):
     :return: Vertex after scaling.
     """
 
-    np_vertex = np.array([[vertex[0]],
-                          [vertex[1]],
-                          [vertex[2]],
-                          [1]])
-
     trans_matrix = np.array([[factor,       0,      0, 0],
                              [     0,  factor,      0, 0],
                              [     0,       0, factor, 0],
                              [     0,       0,      0, 1]])
 
-    return np.matmul(trans_matrix, np_vertex)
+    return np.matmul(trans_matrix, vertex)
 
 
 def translate(vertex, delta_x: float, delta_y: float, delta_z: float):
@@ -133,14 +113,9 @@ def translate(vertex, delta_x: float, delta_y: float, delta_z: float):
     :return: Vertex after translation.
     """
 
-    np_vertex = np.array([[vertex[0]],
-                          [vertex[1]],
-                          [vertex[2]],
-                          [1]])
-
     trans_matrix = np.array([[1, 0, 0, delta_x],
                              [0, 1, 0, delta_y],
                              [0, 0, 1, delta_z],
                              [0, 0, 0,       1]])
 
-    return np.matmul(trans_matrix, np_vertex)
+    return np.matmul(trans_matrix, vertex)
