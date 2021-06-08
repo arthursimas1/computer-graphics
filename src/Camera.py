49365c8cc8ab242951438dfeb39fafc2ebc033ee
Arthur J Simas <arthursimas1@gmail.com>
2021-06-08 10:38:09 -0300

    Implementação inicial do Camera e integração com o wxPython

--- src/Camera.py
+++ src/Camera.py
@@ -1,2 +1,38 @@
-class Camera:
-    pass
+from types import SimpleNamespace
+import wx
+import numpy as np
+import Transforms
+import math
+
+
+class Camera(wx.Panel):
+    def __init__(self, frame, scene):
+        wx.Panel.__init__(self, frame)
+        self.Bind(wx.EVT_PAINT, self.on_paint)
+        self.Bind(wx.EVT_SIZE, self.on_size)
+
+        self.scene = scene
+        self.position = [0, 0, 0]
+        self.look_at = [[1, 0, 0],
+                        [0, 1, 0],
+                        [0, 0, 1]]
+
+    def on_size(self, evt):
+        self.Refresh()
+
+    def on_paint(self, evt):
+        w, h = self.GetClientSize()
+        dc = wx.PaintDC(self)
+        dc.Clear()
+        bmp = self.get_bitmap(w, h)
+        dc.DrawBitmap(bmp, 0, 0)
+
+    def get_bitmap(self, w, h):
+        data = np.zeros((h, w, 3), np.uint8)
+
+        for i in range(w):
+            data[int(math.cos(i / 20) * 20) + h // 2][i] = 90
+            data[int(math.sin(i / 20) * 20) + h // 2][i] = 255
+
+        bmp = wx.Bitmap.FromBuffer(w, h, data)
+        return bmp
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
+from Camera import Camera
 from Object import Object
 from Scene import Scene
+import wx
 
 if __name__ == '__main__':
     s = Scene()
@@ -7,5 +9,13 @@ if __name__ == '__main__':
     obj1 = Object('./3d-obj-examples/coarseTri.fandiskAuto.obj')
     s.add_object(obj1)
 
-    obj2 = Object('./3d-obj-examples/coarseTri.egea1.obj')
-    s.add_object(obj2)
+    # obj2 = Object('./3d-obj-examples/coarseTri.egea1.obj')
+    # s.add_object(obj2)
+
+    app = wx.App()
+    frame = wx.Frame(None, title=f'Computer Graphics Test')
+
+    cam = Camera(frame, s)
+
+    frame.Show()
+    app.MainLoop()
