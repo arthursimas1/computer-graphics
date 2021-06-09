341e0ac2ab0c9c228ad6954bc93a61bd89109307
caio-yabiku <kaiohys@hotmail.com>
2021-06-08 22:13:37 -0300

    Translação e rotação da Camera

--- src/Camera.py
+++ src/Camera.py
@@ -1,4 +1,3 @@
-from types import SimpleNamespace
 import wx
 import numpy as np
 import Transforms
@@ -19,13 +18,14 @@ class Camera(wx.Panel):
         self.Bind(wx.EVT_SIZE, self.on_size)
 
         self.scene = scene
-        self.position = [0, 0, 0]
-        self.look_at = [[1, 0, 0],
-                        [0, 1, 0],
-                        [0, 0, 1]]
-
-        for obj in self.scene.objects:
-            obj.vertexes.sort(key=lambda v: v[2])
+        self.position = np.array([[0.],
+                                  [0.],
+                                  [0.],
+                                  [1.]])
+        self.look_at = np.array([[1., 0., 0., 0.],
+                                 [0., 1., 0., 0.],
+                                 [0., 0., 1., 0.],
+                                 [0., 0., 0., 1.]])
 
     def on_size(self, evt):
         self.Refresh()
@@ -41,11 +41,53 @@ class Camera(wx.Panel):
         data = np.zeros((h, w, 3), np.uint8)
 
         for obj in self.scene.objects:
-            for vt in obj.vertexes:
+            vt = []
+            for i in range(len(obj.vertexes)):
+                vt.append(np.matmul(self.look_at, np.matmul(obj.transformation_matrix, obj.vertexes[i])))
+
+            vt.sort(key=lambda v: v[2][0])
+
+            for v in vt:
                 # data[Y][X]
-                vt = Transforms.scale(vt, 40)[:-1]
-                vt = Transforms.translate(list(map(lambda x: x[0], vt)), 50, -400, 0)[:-1]
-                data[round(vt[1][0])][round(vt[0][0])] = min(round(vt[2][0]), 255)
+                data[round(v[1][0])][round(v[0][0])] = min(round(v[2][0]), 255)
 
         bmp = wx.Bitmap.FromBuffer(w, h, data)
         return bmp
+
+    def translate(self, delta_x: float, delta_y: float, delta_z: float) -> None:
+        """
+        Translate the camera by a given delta.
+
+        :param delta_x: Delta amount to translate the X coordinate.
+        :param delta_y: Delta amount to translate the Y coordinate.
+        :param delta_z: Delta amount to translate the Z coordinate.
+        """
+
+        self.look_at = Transforms.translate(self.look_at, delta_x, delta_y, delta_z)
+
+    def rotate_x(self, deg: float) -> None:
+        """
+        Rotate the camera by a given angle in degrees around X axis.
+
+        :param deg: Degrees to rotate the camera.
+        """
+
+        self.look_at = Transforms.rotate_x(self.look_at, math.radians(deg))
+
+    def rotate_y(self, deg: float) -> None:
+        """
+        Rotate the camera by a given angle in degrees around Y axis.
+
+        :param deg: Degrees to rotate the camera.
+        """
+
+        self.look_at = Transforms.rotate_y(self.look_at, math.radians(deg))
+
+    def rotate_z(self, deg: float) -> None:
+        """
+        Rotate the camera by a given angle in degrees around Z axis.
+
+        :param deg: Degrees to rotate the camera.
+        """
+
+        self.look_at = Transforms.rotate_z(self.look_at, math.radians(deg))
