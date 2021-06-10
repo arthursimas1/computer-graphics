293cf3dce73744b1f09be496ec3d64f4104a5202
Luiz Henrique Teixeira Mendes <luizhteixeiram@gmail.com>
2021-06-09 21:25:19 -0300

    Testando coordenadas normalizadas no main

--- src/main.py
+++ src/main.py
@@ -7,16 +7,18 @@ if __name__ == '__main__':
     s2 = Scene()
 
     obj1 = Object('./3d-obj-examples/coarseTri.fandiskAuto.obj')
-    obj1.scale(20)
+    obj1.translate(1, 0, 0)
+    obj1.scale(50)
     s1.add_object(obj1)
 
     obj2 = Object('./3d-obj-examples/coarseTri.egea1.obj')
-    obj2.scale(200)
+    obj2.translate(2, -1, 0)
+    obj2.scale(50)
     obj2.rotate_y(-45)
     s1.add_object(obj2)
 
     obj3 = Object('./3d-obj-examples/coarseTri.rockerArm.obj')
-    obj3.scale(300)
+    obj3.scale(50)
     obj3.rotate_y(60)
     s2.add_object(obj3)
 
@@ -26,7 +28,7 @@ if __name__ == '__main__':
     cam1.translate(-150, 100, 0)
 
     cam2 = Camera(s1)
-    cam2.translate(100, 50, 0)
+    cam2.translate(100, 0, 0)
 
     cam3 = Camera(s2)
     cam3.translate(-150, -100, 10)
